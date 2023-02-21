f45695e6599624ffccba93659dc00324113ac966
Minkyung Oh <ominomin73@gmail.com>
2023-02-21 22:26:39 +0900

    BlogController

---
+++ src/main/java/com/cos/blog/model/Task.java
@@ -0,0 +1,10 @@
+package com.cos.blog.model;
+
+import lombok.Data;
+
+@Data
+public class Task {
+
+	private String taskName;
+	private String description;
+}
--- src/main/java/com/cos/blog/test/BlogControllerTest.java
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BlogControllerTest {
 
+	//http://localhost:8080/api/test/hello
 	@GetMapping("/api/test/hello")
 	public String hello() {
 		return "<h1>Hello Spring Boot</h1>";
